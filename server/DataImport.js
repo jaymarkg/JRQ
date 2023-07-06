5541150b9b2890189ee74b261ebaaff52201ee78
jaymarkg <jaymarkgonzales09@gmail.com>
2023-07-07 00:57:15 +0800

    jrq files

--- server/DataImport.js
+++ server/DataImport.js
@@ -8,7 +8,7 @@ import asyncHandler from "express-async-handler";
 
 const ImportData = express.Router();
 
-ImportData.post(  "/user",  asyncHandler(
+ImportData.post(  "/users.js",  asyncHandler(
   async (req, res) => {
     await User.deleteOne({});
     const importUser = await User.insertMany(users);
