34cbc74f2212361bb04209846204291107455a89
jaymarkg <jaymarkgonzales09@gmail.com>
2023-07-07 13:00:43 +0800

    jrq files

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use(errorHandler);
 
 
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server run in port ${PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
