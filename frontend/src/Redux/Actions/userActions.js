19bd03dcd1d0b896f7ede32a494ad49e91522793
jmagonzales <qjmagonzales@tip.edu.ph>
2023-07-08 23:58:03 +0800

    commit

--- frontend/src/Redux/Actions/userActions.js
+++ frontend/src/Redux/Actions/userActions.js
@@ -29,7 +29,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `/api/users/login`,
+      `https://jrq-connect.onrender.com/api/users/login`,
       { email, password },
       config
     );
@@ -68,7 +68,7 @@ export const register = (name, email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `/api/users`,
+      `https://jrq-connect.onrender.com/api/users`,
       { name, email, password },
       config
     );
@@ -101,7 +101,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`/api/users/${id}`, config);
+    const { data } = await axios.get(`https://jrq-connect.onrender.com/api/users/${id}`, config);
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     const message =
@@ -134,7 +134,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.put(`/api/users/profile`, user, config);
+    const { data } = await axios.put(`https://jrq-connect.onrender.com/api/users/profile`, user, config);
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
 
